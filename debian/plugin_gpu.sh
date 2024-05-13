4cc0572200793c2e71cca7c12166e97e754b05b9
lesca <lesca4on@outlook.com>
2024-05-13 12:38:48 +0800

    add vulkan gpu support for qualcomm platform

---
+++ debian/plugin_gpu.sh
@@ -0,0 +1,11 @@
+# install software
+install_gpu() {
+    PKG="mesa-vulkan-kgsl_24.1.0-devel-20240120_arm64.deb"
+    PKGS_DIR=$SCRIPT_DIR/../packages
+    [ -f "$PKGS_DIR/$PKG" ] && cp $PKGS_DIR/$PKG $TMPDIR
+    [ -f "$TMPDIR/$PKG" ] && $DISTRO_LOGIN -- apt install -f $TMPDIR/$PKG
+
+    # enable for chromium
+    sed -i "s|Exec=/usr/bin/chromium|Exec=env MESA_LOADER_DRIVER_OVERRIDE=zink TU_DEBUG=noconform /usr/bin/chromium|g" \
+        $DISTRO_ROOTFS/usr/share/applications/chromium.desktop
+}
