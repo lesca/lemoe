c7ab0e37f97d585f09ba5bfa7f7112638c19efe6
lesca <lesca4on@outlook.com>
2024-05-26 19:20:04 +0800

    fix: default browser

--- debian/app_chromium.sh
+++ debian/app_chromium.sh
@@ -7,7 +7,10 @@ install_chromium() {
     sed -i "s|Exec=/usr/bin/chromium|Exec=/usr/bin/chromium --no-sandbox|g" \
         $DISTRO_ROOTFS/usr/share/applications/chromium.desktop
 
+}
+
+setup_user_chromium() {
     # set default browser
-    $DISTRO_LOGIN -- xdg-mime default chromium.desktop x-scheme-handler/https
-    $DISTRO_LOGIN -- xdg-mime default chromium.desktop x-scheme-handler/http
+    $DISTRO_LOGIN --user $DISTRO_USER -- xdg-mime default chromium.desktop x-scheme-handler/https
+    $DISTRO_LOGIN --user $DISTRO_USER -- xdg-mime default chromium.desktop x-scheme-handler/http
 }
