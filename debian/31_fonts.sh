337d05a3d1776dfaa1412ea7bded8049a248978a
lesca <lesca4on@outlook.com>
2024-05-27 23:21:37 +0800

    fix: wps depends on fonts

--- debian/31_fonts.sh
+++ debian/31_fonts.sh
@@ -1,4 +1,4 @@
 install_fonts() {
-    PKGS="fonts-recommended fonts-wqy-* fonts-lxgw-* fonts-cwtex-*" # ttf-mscorefonts-installer"
+    PKGS="fonts-recommended fonts-wqy-* fonts-lxgw-* fonts-cwtex-* ttf-mscorefonts-installer"
     $DISTRO_LOGIN -- bash -c "DEBIAN_FRONTEND=noninteractive apt install -y $PKGS"
 }
