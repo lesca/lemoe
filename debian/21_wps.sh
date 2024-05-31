7cd543efd36c75a6aaba74dd48f37acf7e2de2fc
lesca <lesca4on@outlook.com>
2024-05-31 13:03:48 +0800

    debian: reorder apps; minor fixes

--- debian/21_wps.sh
+++ debian/21_wps.sh
@@ -4,5 +4,5 @@ install_wps() {
     PKGS_DIR=$SCRIPT_DIR/packages
     URL="http://software.openkylin.top/openkylin/yangtze/pool/all/$PKG"
     [ -f "$PKGS_DIR/$PKG" ] && cp $PKGS_DIR/$PKG $TMPDIR || curl --retry 5 -Lo $TMPDIR/$PKG $URL
-    [ -f "$TMPDIR/$PKG" ] && $DISTRO_LOGIN -- apt install -f $TMPDIR/$PKG
+    [ -f "$TMPDIR/$PKG" ] && $DISTRO_LOGIN -- apt install -y -f $TMPDIR/$PKG
 }
